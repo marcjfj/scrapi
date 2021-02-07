b6478dc1fbeff27f4671f00f72d222f99867f2a4
ll-mrac <marc@liftedlogic.com>
2021-02-06 23:38:27 -0600

    looking good

--- client/dist/client.e31bb0bc.js
+++ client/dist/client.e31bb0bc.js
@@ -876,6 +876,7 @@ require("regenerator-runtime/runtime");
 const field = document.querySelector('.blank-field');
 const submitButton = document.querySelector('.submit-button');
 submitButton.addEventListener('click', async () => {
+  const fullUrl = field.value;
   const url = field.value.replace('https://', '').replace(/\//g, '~');
   const response = await fetch(`http://localhost:3000/${url}`);
   const title = await response.json();
--- client/index.js
+++ client/index.js
@@ -3,6 +3,7 @@ const field = document.querySelector('.blank-field');
 const submitButton = document.querySelector('.submit-button');
 
 submitButton.addEventListener('click', async () => {
+  const fullUrl = field.value;
   const url = field.value.replace('https://', '').replace(/\//g, '~');
   const response = await fetch(`http://localhost:3000/${url}`);
   const title = await response.json();
